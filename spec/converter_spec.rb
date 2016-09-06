5b5f8d93796293c07cbec7bc66abc28afeb99bff
nathanallen <thenathanator@gmail.com>
2016-09-06 14:36:27 -0700

    refactor tests to be DRY

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -8,32 +8,23 @@ RSpec.describe Converter do
   end
 
   describe "#convert" do
-    let(:converter){ Converter.new }
+    subject(:converter){ Converter.new }
 
     context "given valid input (positive integers 0..n)" do
-      it "converts 1 to 'I'" do
-        expect( converter.convert 1 ).to eq("I")
+
+      {
+           1 => "I",
+           2 => "II",
+           5 => "V",
+           6 => "VI",
+          10 => "X",
+          11 => "XI"
+      }.each_pair do |i, rn|
+        it "converts #{i} into #{rn}" do
+          expect( converter.convert(i) ).to eq(rn)
+        end
       end
 
-      it "converts 2 to 'II'" do
-        expect( converter.convert 2 ).to eq("II")
-      end
-
-      it "converts 5 to 'V'" do
-        expect( converter.convert 5 ).to eq("V")
-      end
-
-      it "converts 6 to 'VI'" do
-        expect( converter.convert 6 ).to eq("VI")
-      end
-
-      it "converts 10 to 'X'" do
-        expect( converter.convert 10 ).to eq("X")
-      end
-
-      it "converts 11 to 'XI'" do
-        expect( converter.convert 11 ).to eq("XI")
-      end
     end
   end
 end
