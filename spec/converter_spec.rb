5f8ae8aed9ccdd06f7969454a29d97b2098776de
nathanallen <thenathanator@gmail.com>
2016-09-06 11:44:06 -0700

    passing test for #new

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -1,1 +1,9 @@
 require_relative './../converter.rb'
+
+RSpec.describe Converter do
+  describe "#new" do
+    it "initalizes a new Conveter instance" do
+      expect( Converter.new ).to be_an_instance_of Converter
+    end
+  end
+end
