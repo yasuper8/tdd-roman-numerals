88bd79a0227bac76eeb8df98ab1be344adc48c97
nathanallen <thenathanator@gmail.com>
2016-09-06 15:18:12 -0700

    major refactor

--- converter.rb
+++ converter.rb
@@ -2,57 +2,30 @@
 # Converts integers to roman numeral strings
 
 class Converter
+  MAPPINGS = {
+    1000  => "M",
+    900   => "CM",
+    500   => "D",
+    400   => "CD",
+    100   => "C",
+    90    => "XC",
+    50    => "L",
+    40    => "XL",
+    10    => "X",
+    9     => "IX",
+    5     => "V",
+    4     => "IV",
+    1     => "I",
+  }
+
   def convert(n)
     output = ""
 
-    thousands = n/1000
-    output << "M" * thousands
-    n -= 1000 * thousands
+    MAPPINGS.each_pair do |divisor, symbol|
+      count, n = n.divmod(divisor)
+      output << symbol * count
+    end
 
-    ninehundreds = n/900
-    output << "CM" * ninehundreds
-    n -= 900 * ninehundreds
-
-    fivehundreds = n/500
-    output << "D" * fivehundreds
-    n -= 500 * fivehundreds
-
-    fourhundreds = n/400
-    output << "CD" * fourhundreds
-    n -= 400 * fourhundreds
-
-    hundreds = n/100
-    output << "C" * hundreds
-    n -= 100 * hundreds
-
-    nineties = n/90
-    output << "XC" * nineties
-    n -= 90 * nineties
-
-    fifties = n/50
-    output << "L" * fifties
-    n -= 50 * fifties
-
-    forties = n/40
-    output << "XL" * forties
-    n -= 40 * forties
-
-    tens = n/10
-    output << "X" * tens
-    n -= 10 * tens
-
-    nines = n/9
-    output << "IX" * nines
-    n -= 9 * nines
-
-    fives = n/5
-    output << "V" * fives
-    n -= 5 * fives
-
-    fours = n/4
-    output << "IV" * fours
-    n -= 4 * fours
-
-    output << "I" * n
+    output
   end
 end
